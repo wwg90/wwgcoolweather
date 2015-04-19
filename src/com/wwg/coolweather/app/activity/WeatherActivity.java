9501ac5cffd2aea6b773ee752e190e8b4175c730
wwg <172051826@qq.com>
2015-04-19 11:07:39 +0800

    新增切换城市和手动更新天气的功能

--- src/com/wwg/coolweather/app/activity/WeatherActivity.java
+++ src/com/wwg/coolweather/app/activity/WeatherActivity.java
@@ -1,21 +1,24 @@
 package com.wwg.coolweather.app.activity;
 
-import com.wwg.coolweather.app.R;
-import com.wwg.coolweather.app.util.HttpCallBackListener;
-import com.wwg.coolweather.app.util.HttpUtil;
-import com.wwg.coolweather.app.util.Utility;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class WeatherActivity extends Activity {
+import com.wwg.coolweather.app.R;
+import com.wwg.coolweather.app.util.HttpCallBackListener;
+import com.wwg.coolweather.app.util.HttpUtil;
+import com.wwg.coolweather.app.util.Utility;
+
+public class WeatherActivity extends Activity implements OnClickListener{
 	private LinearLayout weatherInfoLayout;
 	/**
 	 * 用于显示城市名
@@ -41,7 +44,17 @@ public class WeatherActivity extends Activity {
 	 * 用于显示当前日期
 	 */
 	private TextView currentDateText;
-
+	
+	/**
+	 * 切换城市按钮
+	 */
+	private Button switchCity;
+	/**
+	 * 更新天气按钮
+	 */
+	private Button refreshWeather;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -55,7 +68,7 @@ public class WeatherActivity extends Activity {
 		weatherDespText = (TextView) findViewById(R.id.weather_desp);
 		temp1Text = (TextView) findViewById(R.id.temp1);
 		temp2Text = (TextView) findViewById(R.id.temp2);
-		currentDateText = (TextView) findViewById(R.id.city_name);
+		currentDateText = (TextView) findViewById(R.id.current_date);
 
 		String countyCode = getIntent().getStringExtra("county_code");
 		if (!TextUtils.isEmpty(countyCode)) {
@@ -65,6 +78,11 @@ public class WeatherActivity extends Activity {
 			cityNameText.setVisibility(View.INVISIBLE);
 			queryWeatherCode(countyCode);
 		}
+		
+		switchCity = (Button) findViewById(R.id.switch_city);
+		refreshWeather = (Button) findViewById(R.id.refresh_weather);
+		switchCity.setOnClickListener(this);
+		refreshWeather.setOnClickListener(this);
 
 	}
 
@@ -159,6 +177,32 @@ public class WeatherActivity extends Activity {
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
 
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.switch_city:
+			Intent intent = new Intent(WeatherActivity.this,ChooseAreaActivity.class);
+			intent.putExtra("from_weather_activity", true);
+			startActivity(intent);
+			finish();
+			break;
+			
+		case R.id.refresh_weather:
+			publishText.setText("同步中...");
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			String weatherCode = prefs.getString("weather_code", "");
+			if(!TextUtils.isEmpty(weatherCode)){
+				queryWeatherInfo(weatherCode);
+			}
+			
+			break;
+
+		default:
+			break;
+		}
+		
 	}
 
 }
